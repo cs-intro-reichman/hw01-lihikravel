5139407952f39441cc0714436aa58aa4640b8c19
lihikravel <154433549+lihikravel@users.noreply.github.com>
2023-12-20 14:55:46 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -4,7 +4,7 @@ public class Triangle {
         int b = Integer.parseInt(args[1]);
         int c = Integer.parseInt(args[2]);
         boolean x = (a + b > c && a + c >b && c+b>a);
-        System.out.println(a + " , " + b + " , " + c + " : " + x);
+        System.out.println(a + ", " + b + ", " + c + ": " + x);
           
     }
 }
