59c9605cc1ad398f62af359e66ecc5dcbed3ce0e
lihikravel <154433549+lihikravel@users.noreply.github.com>
2023-12-20 14:50:40 +0200

    solution

--- LinearEq.java
+++ LinearEq.java
@@ -1,9 +1,12 @@
-/*
- * Solves linear equations of the form a⋅x + b = c.
- * The program gets a, b, and c as command-line arguments,
- * computes x, and prints the result.
- * Treats the three arguments as well as the computed value as double values
- */
 public class LinearEq {
-	// Put your code here
+    public static void main(String[] args) {
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        double c = Double.parseDouble(args[2]);
+        System.out.println(a + " * " + "x " + "+ " + b + " = " + c);
+        double midSum = c - b;
+        double x = midSum / a; 
+          
+        System.out.println("x " + "= " + x );
+    }
 }
