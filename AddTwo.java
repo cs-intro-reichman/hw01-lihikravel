ffa67eefd400f98002e6a5620bded7dbf0dcf252
lihikravel <lihi.kravel@runi.ac.il>
2023-12-21 21:42:44 +0200

    Add awesome code for Homework 1

---
+++ AddTwo.java
@@ -0,0 +1,8 @@
+public class AddTwo {
+    public static void main(String[] args) {
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        int sum = num1 + num2;
+        System.out.println(num1 + " + " + num2 + " = " + sum);        
+    }
+}
