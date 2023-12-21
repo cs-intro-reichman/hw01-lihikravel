82699e1882e11fb6b94126c59580ed2911445a23
lihikravel <lihi.kravel@runi.ac.il>
2023-12-21 22:02:41 +0200

    change

---
+++ Triangle.java
@@ -0,0 +1,10 @@
+public class Triangle {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+        boolean x = (a + b > c && a + c >b && c+b>a);
+        System.out.println(a + ", " + b + ", " + c + ": " + x);
+          
+    }
+}
